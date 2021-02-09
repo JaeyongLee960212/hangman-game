9500e59c5239c50a3097f6fa25e37d3e9cbbbc93
JaeyongLee960212 <76797284+JaeyongLee960212@users.noreply.github.com>
2021-02-09 20:46:19 +0900

    Create hangman file

---
+++ index.js
@@ -0,0 +1,17 @@
+var words = [
+    "javascript",
+    "monkey",
+    "amazing",
+    "pancake"
+];
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+var answerArray= [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+
+var remainingLetters = word.length;
+
+while (remainingLetters > 0) {};
