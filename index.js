664d3dc8382c25af8cf67776bcae4849b696676b
JaeyongLee960212 <76797284+JaeyongLee960212@users.noreply.github.com>
2021-02-13 22:07:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//단어 배열 제조
 var words = [
     "javascript",
     "monkey",
@@ -5,8 +6,10 @@ var words = [
     "pancake"
 ];
 
+//임의의 단어 고르기
 var word = words[Math.floor(Math.random() * words.length)];
 
+//정답 배열 설정
 var answerArray= [];
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_";
@@ -14,4 +17,26 @@ for (var i = 0; i < word.length; i++) {
 
 var remainingLetters = word.length;
 
-while (remainingLetters > 0) {};
+//게임 반복문
+while (remainingLetters > 0) {
+    alert(answerArray.join(" "));
+
+    var guess = prompt("글자를 입력해주세요! 취소를 누르면 게임을 중단합니다");
+    if (guess === null) {
+        break;
+    } else if (guess.length !== 1) {
+        alert("한 글자만 입력해주세요!");
+    } else {
+        for (var j = 0; j < word.length; j++) {
+            if(word[j] === guess) {
+                answerArray[j] = guess;
+                remainingLetters--;
+            }
+        }
+    }
+}
+//게임 반복문 끝
+
+//정답 출력 및 축하 메세지
+alert(answerArray.join(" "));
+alert("축하합니다! 정답은 " + word + "입니다.");
